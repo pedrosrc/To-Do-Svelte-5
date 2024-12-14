593e0c16fd3e97aec1a1e2e3563499581e269475
pedrosrc <pedroleon.pc@outlook.com>
2024-12-14 12:09:59 -0300

    feat: add Form

--- src/lib/components/Form.svelte
+++ src/lib/components/Form.svelte
@@ -1,46 +1,46 @@
 <script lang="ts">
-
-	interface Props {
-		title: string, 
-		priority: string;
-		project: string;
-	}
+  import { type Props } from "$lib";
+  import { createTask } from "$lib/stores/tasksControll";
 
 	let data: Props = { 
+    id: 0,
 		title: '', 
 		priority: '',
 		project: '',
+		done: false
 	};
+
+  function handleSubmit() {
+    createTask(data)
+  }
 	
 	const values:  { label: string, value: string }[] = [
 		{ label: 'Low', value: 'low' },
 		{ label: 'Mid', value: 'mid' },
 		{ label: 'High', value: 'high' }
 	];
-	let selected = values[1];
-
 </script>
 
-<form method="POST" class="flex flex-col p-4 gap-3 bg-neutral rounded-lg">
+<form on:submit={handleSubmit} class="flex flex-col p-4 gap-3 bg-neutral rounded-lg">
 	<label class="flex items-center gap-2 text-lg font-medium text-gray-900">
 		Task:
-		<input name="task" type="text" value={data.title} class="text-blac shadow-md rounded-full border-none outline-none bg-white focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
+		<input name="task" type="text"  bind:value={data.title} class="text-blac shadow-md rounded-full border-none outline-none bg-white focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
 	</label>
 	<label class="flex items-center gap-2 text-lg font-medium text-gray-900">
 		Priority:
 		{#each values as value}
 			<input
 				type="radio"
-				bind:group={selected}
-				class="peer h-5 w-5 cursor-pointer appearance-none rounded-full border bg-white border-red-600 checked:border-red-600 transition-all"
+				class="peer h-5 w-5 cursor-pointer appearance-none rounded-full border bg-white transition-all"
 				name="priority"
-				value={data.priority}
+        value={value.value}
+        bind:group={data.priority}
 			> {value.label}
 		{/each}
 	</label>
 	<label class="flex items-center gap-2 text-lg font-medium text-gray-900">
 		Project:
-		<input name="project" type="text" value={data.project}  class="text-blac shadow-md rounded-full border-none outline-none bg-white focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
+		<input name="project" type="text" bind:value={data.project}  class="text-blac shadow-md rounded-full border-none outline-none bg-white focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
 	</label>
-	<button type="submit">Create</button>
+	<button class="bg-dark-blue rounded-full p-2 text-center text-white" type="submit">Create</button>
 </form>
