e45e31aa7c3f42b8c536c4e284d31c9d26da775f
pedrosrc <pedroleon.pc@outlook.com>
2024-12-14 12:09:33 -0300

    feat: add type Props

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,8 @@
 // place files you want to import through the `$lib` alias in this folder.
+export type Props = {
+  id: number,
+  title: string, 
+  priority: string;
+  project: string;
+  done: boolean;
+}
